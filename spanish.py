39cc46c08f798d3e9627c2231123a65eafecca66
Kai Lacerna <iruelelacerna787>
2022-03-21 15:56:51 -0400

    + spanish.py and spanish.html

---
+++ spanish.py
@@ -0,0 +1,23 @@
+englishWord = input("Give me a word in english... ")
+
+if englishWord == "cat":
+    print("gato")
+elif englishWord == "dog":
+    print("perro")
+elif englishWord == "horse":
+    print("caballo")
+else:
+    print("no entiendo")
+
+#if(englishWord == "cat"){
+#    console.log("gato")
+#    }
+#        else if(englishWord == "dog"){
+#            console.log("perro")
+#        }
+#        else if(englishWord == "horse"){
+#            console.log("caballo")
+#        }
+#        else{
+#            console.log("no entiendo")
+#        }
