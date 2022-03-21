21f299e2a84dcee87b406cc7dcf04b13b45c53fa
Kai Lacerna <iruelelacerna787>
2022-03-21 15:46:10 -0400

    + cake.py program with all-three conditions

---
+++ cake.py
@@ -0,0 +1,8 @@
+cakeAnswer = input("Do you want some cake? ")
+
+if(cakeAnswer == "yes"):
+    print("Have some cake. ")
+elif(cakeAnswer == "no"):
+    print("No cake for you. ")
+else:
+    print("I don't understand. ")
